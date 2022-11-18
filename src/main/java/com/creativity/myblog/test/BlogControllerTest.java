b8bbec34e44b196b4cab54d17564a685f31534ae
parkjunho0708 <jhpark870708@daum.net>
2022-11-18 23:37:14 +0900

    commit test

--- src/main/java/com/creativity/myblog/test/BlogControllerTest.java
+++ src/main/java/com/creativity/myblog/test/BlogControllerTest.java
@@ -3,6 +3,7 @@ package com.creativity.myblog.test;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// spring
 // 스프링이 com.creativity.myblog 패키지 이하를 스캔해서 모든 파일을 메모리에 new 하는 것은 아니구요.
 // 특정 어노테이션이 붙어있는 클래스 파일들을 new해서 (IoC) 스프링 컨테니어네 관리해줍니다.
 @RestController
