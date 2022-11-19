7bfb2173a95db8564fa9e17d429d2600361de700
parkjunho0708 <jhpark870708@daum.net>
2022-11-19 12:26:04 +0900

    thymeleaf 셋팅 및 화면 리스트 출력

--- src/main/java/com/creativity/myblog/test/ThymeleafControllerTest.java
+++ src/main/java/com/creativity/myblog/test/ThymeleafControllerTest.java
@@ -1,19 +1,33 @@
 package com.creativity.myblog.test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ThymeleafControllerTest {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public String thymeleafIndexExample(Model model) {
-		model.addAttribute("name", "creation");
-		model.addAttribute("values", Arrays.asList("Spring", "Spring Boot", "Vue.js", "React"));
-		return "/index";
+	@RequestMapping(value = "/thymeleaf/example", method = RequestMethod.GET)
+	public ModelAndView thymeleafIndexExample() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("name", "creation");
+		
+		List<String> list = new ArrayList<String>();
+	    list.add("List A");
+	    list.add("List B");
+	    list.add("List C");
+	    list.add("List D");
+	    list.add("List 1");
+	    list.add("List 2");
+	    list.add("List 3");
+	    
+		modelAndView.addObject("values", list);
+		modelAndView.setViewName("index");
+		return modelAndView;
 	}
 }
